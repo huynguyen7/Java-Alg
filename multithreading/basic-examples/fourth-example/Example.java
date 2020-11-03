8c0b1406fa1ed52aac012b797ac187dd70b7f46a
Nguyễn Huy <huy.nguyen712sp@gmail.com>
2020-11-02 21:57:20 -0800

    Adding more codes

---
+++ multithreading/basic-examples/fourth-example/Example.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Example {
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+		int n = Integer.parseInt(args[0]);
+		
+		int[] nums = generateRandomNumberArr(n);
+		printTimeSerial(nums);
+		printTimeParallel(nums);
+	}
+
+	private static void printTimeSerial(int[] nums) {
+		double startTime = System.nanoTime();
+		int sum1 = Arrays.stream(nums)
+						.sum();
+		int sum2 = Arrays.stream(nums)
+						.sum();
+		double endTime = System.nanoTime();
+		System.out.println("Time taken with serial: " + (startTime - endTime) + " ms.");
+		System.out.println("Sum = " + (sum1 + sum2) + "\n");
+	}
+
+	// Fork another thread to run in parallel.
+	private static void printTimeParallel(int[] nums) throws ExecutionException, InterruptedException {
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		Callable<Integer> task = () -> {
+			return Arrays.stream(nums)
+						.sum();
+		};
+
+		double startTime = System.nanoTime();
+		Future<Integer> future = service.submit(task);
+		int sum1 = Arrays.stream(nums)
+						.sum();
+		int sum2 = future.get().intValue();
+		double endTime = System.nanoTime();
+		System.out.println("Time taken with parallel: " + (startTime - endTime) + " ms.");
+		System.out.println("Sum = " + (sum1 + sum2) + "\n");
+
+		service.shutdown();
+	}
+
+	private static int[] generateRandomNumberArr(int n) {
+		int[] nums = new int[n];
+		Random rand = new Random();
+		for(int i = 0; i < nums.length; ++i)
+			nums[i] = rand.nextInt(10);
+		return nums;
+	}
+}
