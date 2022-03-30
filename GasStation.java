5c392a63c05b3791558ba9427812a2096f8337e1
Huy Nguyen <huynguyen@Huys-MacBook-Air.local>
2022-03-30 15:07:40 -0700

    Adding more codes

--- GasStation.java
+++ GasStation.java
@@ -5,20 +5,22 @@ public class GasStation {
     public static void main(String[] args) {
         int[] gas1 = {1,2,3,4,5};
         int[] cost1 = {3,4,5,1,2};
-        showResults(gas1, cost1); // expect 3
+        assert(showResults(gas1, cost1) == 3); // expect 3
 
         int[] gas2 = {2,3,4};
-        int[] cost2 = {3,4,4};
-        showResults(gas2, cost2); // expect 3
+        int[] cost2 = {3,4,3};
+        assert(showResults(gas2, cost2) == -1); // expect -1
 
         int[] gas3 = {5,1,2,3,4};
         int[] cost3 = {4,4,1,5,1};
-        showResults(gas3, cost3); // expect 4
+        assert(showResults(gas3, cost3) == 4); // expect 4
     }
 
-    private static void showResults(int[] gas, int[] cost) {
+    private static int showResults(int[] gas, int[] cost) {
         System.out.println("\t----ShowResults----");
-        System.out.printf("GAS: %s\nCOST: %s\nOUTPUT: %d\n\n", Arrays.toString(gas), Arrays.toString(cost), canCompleteCircuit(gas, cost));
+        int output = canCompleteCircuit(gas, cost);
+        System.out.printf("GAS: %s\nCOST: %s\nOUTPUT: %d\n\n", Arrays.toString(gas), Arrays.toString(cost), output);
+        return output;
     }
 
     // Time: O(n), space: O(1)
@@ -30,7 +32,7 @@ public class GasStation {
         int i;
         int start = -1, tank = 0;
 
-        for(i = 0; i < n; ++i) {
+        for(i = 0; i < n; ++i) { // Find the best result at first loop, since the answer is unique, there is only one result else nothing.
             tank += gas[i]-cost[i];
             if(tank < 0) {
                 tank = 0;
@@ -38,11 +40,36 @@ public class GasStation {
             } else if(start == -1) start = i;
         }
 
-        for(i = 0 ; i < start; ++i) {
+        for(i = 0 ; i < start; ++i) { // Check whether if the best result works.
             tank += gas[i]-cost[i];
             if(tank < 0) return -1;
         }
 
         return start;
+    }
+
+    // Naive approach
+    // Time: O(n^2), space: O(1)
+    public static int canCompleteCircuitII(int[] gas, int[] cost) {
+        final int n = gas.length;
+        for(int i = 0; i < n; ++i) {
+            int j = i;
+            int counter = n;
+            boolean flag = true;
+            int currGas = gas[j];
+            while(counter-- > 0) {
+                currGas = currGas - cost[j];
+                if(currGas < 0) {
+                    flag = false;
+                    break;
+                }
+                j = (j+1) % n;
+                currGas += gas[j];
+            }
+            
+            if(flag) return i;
+        }
+        
+        return -1;
     }
 }
--- HouseRobber.java
+++ HouseRobber.java
@@ -4,23 +4,28 @@ import java.util.Arrays;
 public class HouseRobber {
 	public static void main(String[] args) {
 		int[] nums1 = {1,2,3,1};
-		showResults(nums1); // expect 4
+		assert(showResults(nums1) == 4); // expect 4
 
 		int[] nums2 = {2,7,9,3,1};
-		showResults(nums2); // expect 12
+		assert(showResults(nums2) == 12); // expect 12
 
 		int[] nums3 = {1,2,3,4};
-		showResults(nums3); // expect 6
+		assert(showResults(nums3) == 6); // expect 6
+
+        int[] nums4 = {3,2,2,10,2};
+        assert(showResults(nums4) == 13); // expect 13
 	}
 	
-	private static void showResults(int[] nums) {
+	private static int showResults(int[] nums) {
 		System.out.println("----ShowResults----");
 		System.out.println(Arrays.toString(nums));
-		int rs = rob(nums);
+		int rs = robI(nums);
 		System.out.printf("Result: %d\n\n", rs);
+        
+        return rs;
 	}
 
-	// DP approach
+	// DP approach, bottom-up, no space consumption.
 	// Time: O(n), space: O(1)
 	public static int rob(int[] nums) {
 		// rob1 is to hold the previous house's max sum value.
@@ -35,4 +40,40 @@ public class HouseRobber {
 
 		return rob2;
 	}
+
+    // DP approach, top-down.
+    // Time: O(n), space: O(n)
+    public static int robI(int[] nums) {
+        int[] mem = new int[nums.length] ;
+        Arrays.fill(mem, -1);
+
+        return dfsI(nums, mem, 0);
+    }
+
+    private static int dfsI(int[] nums, int[] mem, int curr) {
+        if(curr >= nums.length) return 0;
+        else if(mem[curr] != -1) return mem[curr];
+        else {
+            int take = dfsI(nums, mem, curr+2) + nums[curr];
+            int notTake = dfsI(nums, mem, curr+1);
+
+            mem[curr] = Math.max(take, notTake);
+            return mem[curr];
+        }
+    }
+
+    // Recursive approach.
+    // Time: O(2^n), space: O(2^n)
+    public static int robII(int[] nums) {
+        return dfsII(nums, 0);
+    }
+    
+    private static int dfsII(int[] nums, int curr) {
+        if(curr >= nums.length) return 0;
+        
+        int take = dfsII(nums, curr+2) + nums[curr];
+        int notTake = dfsII(nums, curr+1);
+        
+        return Math.max(take, notTake);
+    }
 }
